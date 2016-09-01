8ea343c917241fdee2c5e921e9f97ab472e8e453
Krzysztof Magiera <krzysztof.magiera@codeconcept.pl>
2016-09-01 14:15:52 +0200

    validation for orders & improved views

--- Repository/Models/Order.cs
+++ Repository/Models/Order.cs
@@ -13,17 +13,19 @@ namespace Repository.Models
         {
             this.Items = new HashSet<OrderItem>();
         }
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        //public int Id { get; set; }
         [Display(Name = "Order Id:")]
         public string OrderId { get; set; }
         [Display(Name = "Order date:")]
+        [Required(ErrorMessage = "Order date is required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public System.DateTime OrderDate { get; set; }
         [Display(Name = "Order value:")]
+        [Required(ErrorMessage = "Order Value is required")]
         public string OrderValue { get; set; }
         public virtual ICollection<OrderItem> Items { get; set; }
+        [Display(Name = "User Id:")]
+        [Required(ErrorMessage = "User Id is required")]
         public string UserId { get; set; }
         public virtual User User { get; set; }
     }
